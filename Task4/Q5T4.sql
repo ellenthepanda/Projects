98db0319038b47923ec3551f9a9c2e3e3e18e732
Ellen Muller <>
2020-04-15 17:59:28 +0100

    Updated Q5 to include the whole question as one sql query

--- Task4/Q5T4.sql
+++ Task4/Q5T4.sql
@@ -12,3 +12,23 @@ SELECT COUNT(billing_amount_captured) AS num_of_txns_over_100gbp FROM curve.anal
 #How many successful transactions
 SELECT COUNT(funding_card_capture_status) AS num_of_successful_txns FROM curve.analytics.transaction WHERE (user_id=643181 AND funding_card_capture_status = 'APPROVED');
 
+SELECT
+    (SELECT COUNT(*)
+    FROM curve.analytics.transaction
+    WHERE (user_id=643181)) AS num_of_transactions,
+
+    (SELECT COUNT(pos_auth_currency_iso)
+    FROM curve.analytics.transaction
+    WHERE (user_id=643181 AND pos_auth_currency_iso != 'GBP')) AS num_of_txns_over_100gbp,
+
+    (SELECT COUNT(billing_amount_captured)
+    FROM curve.analytics.transaction
+    WHERE (user_id=643181 AND billing_amount_captured >10000)) AS num_of_txns_over_100gbp,
+
+    (SELECT COUNT(funding_card_capture_status) AS num_of_successful_txns
+    FROM curve.analytics.transaction
+    WHERE (user_id=643181 AND funding_card_capture_status = 'APPROVED')) AS num_of_successful_txns
+
+FROM curve.analytics.transaction
+GROUP BY num_of_transactions;
+#This outputs the same results but in one query
